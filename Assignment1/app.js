7abf1e7081f8bc68b2745c4d4194b05f62b6cbb2
Damon-Jo <lunaticjky@gmail.com>
2022-10-07 13:37:49 -0700

    created assignment1 and added the get request for using query param

---
+++ Assignment1/app.js
@@ -0,0 +1,174 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const https = require('https')
+const { get } = require('http')
+
+const app = express()
+const port = 5000
+
+//var pokemonModel;
+var pokemonTypes;
+var pokemons;
+var pokemonModel;
+
+app.use(express.json())
+
+
+
+
+
+app.listen(process.env.PORT || port, async () => {
+  try {
+    // mongoose.connect('mongodb+srv://lunaticky:rhanchd6@cluster0.rfmec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    mongoose.connect('mongodb+srv://lunaticky:rhanchd6@cluster0.rfmec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'); 
+
+  } catch (error) {
+    console.log('db error');
+  }
+
+  var data = "";
+  https.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/types.json", async (res) => {
+    res.on('data', (chunk) => {
+      data += chunk // aggregate data
+    });
+    res.on('end', () => {
+      // once data is completly fetched do JSON.parse();
+      console.log('No more data in response.')
+      // pokemonTypes = JSON.parse(data);
+      pokemonTypes = JSON.parse(data).map(type => {
+        return type.english;
+      })
+      // console.log(pokemonTypes)
+    })
+  }).on('error', (e) => {
+    // listen for error 
+    console.log(e.message);
+  });
+
+
+
+  const { Schema } = mongoose;
+
+  const pokemonSchema = new Schema({
+    "id": {
+      type: Number,
+      unique: true
+    },
+    "name": {
+      "english": {
+        type: String,
+        maxlength: 20
+      },
+      "japanese": {
+        type: String
+      },
+      "chinese": {
+        type: String
+      },
+      "french": {
+        type: String
+      }
+    },
+    "type": {
+      type: [String],
+      enum: pokemonTypes
+    },
+    "base": {
+      "HP": Number,
+      "Attack": Number,
+      "Defense": Number,
+      "Sp. Attack": Number,
+      "Sp. Defense": Number,
+      "Speed": Number
+    }
+  });
+
+  pokemonModel = mongoose.model("pokemons", pokemonSchema)
+
+  var chunks = ""
+  https.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json", async (res) => {
+
+    res.on('data', (chunk) => {
+      chunks += chunk;
+    });
+    res.on('end', async () => {
+      // once data is completly fetched do JSON.parse();
+      console.log('No more data in response.')
+      pokemons = JSON.parse(chunks);
+
+
+      let product = await pokemonModel.exists({id: 1});
+
+      if(product){
+        pokemonModel.collection.drop();
+        pokemonModel.create(pokemons);
+      } else {
+        pokemonModel.create(pokemons);
+      }
+
+
+      console.log("pokemons are loaded into database");
+      // pokemons.forEach((pokemon)=>{
+      //   const newPokemon = new pokemonModel(pokemon);
+      //   newPokemon.save();
+      // });
+    })
+  })
+
+
+
+
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+// 1 - establish the connectino the db
+
+// 2 - create the schema
+
+// 3 - create the model
+
+// 4 - populate the db with pokemons
+
+app.get('/', (req, res) => {
+  // res.send(pokemonTypes)
+  res.send(pokemonTypes);
+})
+
+// app.get('/api/v1/pokemons?count=2&after=10')     // - get all the pokemons after the 10th. List only Two.
+app.get('/api/v1/pokemons', function(req, res) {
+  console.log(req.query);
+  var after = null
+  var count = null
+
+  if(req.query){
+    after = {id: {$gt: parseInt(req.query.after)}}
+    console.log(after)
+    count = parseInt(req.query.count)
+
+
+    pokemonModel.find(after).limit(count)
+    .then((doc)=>{
+
+      if (doc.length == 0){
+        return res.status(200).json({ msg : "Error : pass pokemon id between 1 and 811"});
+        // res.json({value:"Cast Error: pass pokemon id between 1 and 811"})
+      } else {
+        console.timeLog(doc)
+        res.json(doc)
+      }
+    })
+    .catch(err=>{
+      return res.status(500).json({status: "ServerErrror", errMsg: "Error : when getting a pokemon"})
+    })
+
+  }else {
+          pokemonModel.find({})
+              .then(docs => { lengthChecker(res, docs) })
+              .catch(err => {
+                  console.error(err)
+                  res.json({ msg: "Error returning all pokemon" })
+          })
+      }
+})
+
