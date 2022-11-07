3d141214f7bbaca9c223e3ceb12c7fe6374ad579
Damon-Jo <lunaticjky@gmail.com>
2022-11-07 01:46:39 -0800

    created the week8 lab

--- week8_lab/app.js
+++ week8_lab/app.js
@@ -9,124 +9,115 @@ const regexForm = /([a-zA-Z]*)(\$[a-z]*)([0-9]*)/
 var pokemonTypes;
 var pokemons;
 var pokemonModel;
+const { handleErr } = require("./errorHandler.js")
+
+const { asyncWrapper } = require("./asyncWrapper.js")
+const {
+  PokemonBadRequest,
+  PokemonBadRequestMissingID,
+  PokemonBadRequestMissingAfter,
+  PokemonDbError,
+  PokemonNotFoundError,
+  PokemonDuplicateError,
+  PokemonNoSuchRouteError,
+  PokemonNameLengthError,
+  PokemonCastError
+} = require("./errors.js")
 
 app.use(express.json())
 
-app.listen(process.env.PORT || port, async () => {
-  try {
+const start =asyncWrapper(async ()=> {
+  app.listen(process.env.PORT || port, async (err) => {
+
     await mongoose.connect('mongodb+srv://lunaticky:rhanchd6@cluster0.rfmec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'); 
+    if(err)
+      throw new PokemonDbError(err)
 
-  } catch (error) {
-    console.log('db error');
-  }
-
-  var data = "";
-  https.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/types.json", async (res) => {
-    res.on('data', (chunk) => {
-      data += chunk // aggregate data
+  
+    var data = "";
+    https.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/types.json", async (res) => {
+      res.on('data', (chunk) => {
+        data += chunk // aggregate data
+      });
+      res.on('end', () => {
+        console.log("The types of pokemons are loaded from the url.")
+        pokemonTypes = JSON.parse(data).map(type => {
+          return type.english;
+        })
+      })
+    }).on('error', (e) => {
+      // listen for error 
+      console.log(e.message);
     });
-    res.on('end', () => {
-      console.log("The types of pokemons are loaded from the url.")
-      pokemonTypes = JSON.parse(data).map(type => {
-        return type.english;
+  
+    const { Schema } = mongoose;
+    const pokemonSchema = new Schema({
+      "id": {
+        type: Number,
+        unique: true
+      },
+      "name": {
+        "english": {
+          type: String,
+          maxlength: 20
+        },
+        "japanese": {
+          type: String
+        },
+        "chinese": {
+          type: String
+        },
+        "french": {
+          type: String
+        }
+      },
+      "type": {
+        type: [String],
+        enum: pokemonTypes
+      },
+      "base": {
+        "HP": Number,
+        "Attack": Number,
+        "Defense": Number,
+        "Sp. Attack": Number,
+        "Sp. Defense": Number,
+        "Speed": Number
+      }
+    });
+  
+    pokemonModel = mongoose.model("pokemons", pokemonSchema)
+  
+    var chunks = ""
+    https.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json", async (res) => {
+  
+      res.on('data', (chunk) => {
+        chunks += chunk;
+      });
+      res.on('end', async () => {
+        pokemons = JSON.parse(chunks);
+  
+  
+        pokemonModel.count(function (err, count) {
+          if (!err && count === 0) {
+            pokemonModel.create(pokemons);
+          } else {
+            pokemonModel.collection.drop();
+            pokemonModel.create(pokemons);
+          }
+      });
+  
+        console.log("pokemons are loaded into database");
+        // pokemons.forEach((pokemon)=>{
+        //   const newPokemon = new pokemonModel(pokemon);
+        //   newPokemon.save();
+        // });
       })
     })
-  }).on('error', (e) => {
-    // listen for error 
-    console.log(e.message);
-  });
-
-  const { Schema } = mongoose;
-  const pokemonSchema = new Schema({
-    "id": {
-      type: Number,
-      unique: true
-    },
-    "name": {
-      "english": {
-        type: String,
-        maxlength: 20
-      },
-      "japanese": {
-        type: String
-      },
-      "chinese": {
-        type: String
-      },
-      "french": {
-        type: String
-      }
-    },
-    "type": {
-      type: [String],
-      enum: pokemonTypes
-    },
-    "base": {
-      "HP": Number,
-      "Attack": Number,
-      "Defense": Number,
-      "Sp. Attack": Number,
-      "Sp. Defense": Number,
-      "Speed": Number
-    }
-  });
-
-  pokemonModel = mongoose.model("pokemons", pokemonSchema)
-
-  var chunks = ""
-  https.get("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json", async (res) => {
-
-    res.on('data', (chunk) => {
-      chunks += chunk;
-    });
-    res.on('end', async () => {
-      pokemons = JSON.parse(chunks);
-
-
-      pokemonModel.count(function (err, count) {
-        if (!err && count === 0) {
-          pokemonModel.create(pokemons);
-        } else {
-          pokemonModel.collection.drop();
-          pokemonModel.create(pokemons);
-        }
-    });
-
-    // pokemons.map(element => {
-    //   //insert to db
-    //   // console.log(element);
-    //   element["base"]["Speed Attack"] = element["base"]["Sp. Attack"];
-    //   delete element["base"]["Sp. Attack"];
-    //   element["base"]["Speed Defense"] = element["base"]["Sp. Defense"];
-    //   delete element["base"]["Sp. Defense"];
-    //   pokemonModel.findOneAndUpdate({id: element["id"]}, {}, { upsert: true, new: true }, function (err, result) {
-    //     if (err) console.log("11111111" + err);
-    //     // saved!
-    //     // console.log(result);
-    //   });
-    // })
-
-
-
-      // let product = await pokemonModel.exists({id: 1});
-
-      // if(product){
-      //   pokemonModel.collection.drop();
-      //   pokemonModel.create(pokemons);
-      // } else {
-      //   pokemonModel.create(pokemons);
-      // }
-
-
-      console.log("pokemons are loaded into database");
-      // pokemons.forEach((pokemon)=>{
-      //   const newPokemon = new pokemonModel(pokemon);
-      //   newPokemon.save();
-      // });
-    })
+    console.log(`Example app listening on port ${port}`)
   })
-  console.log(`Example app listening on port ${port}`)
 })
+
+start();
 
 
 // 1 - establish the connectino the db
@@ -139,39 +130,37 @@ app.get('/', (req, res) => {
 })
 
 // app.get('/api/v1/pokemons?count=2&after=10')     // - get all the pokemons after the 10th. List only Two.
-app.get('/api/v1/pokemons', function(req, res) {
+app.get('/api/v1/pokemons', asyncWrapper(async (req, res)=> {
   var after = null
   var count = null
 
   var size = Object.keys(req.query).length
+  console.log(size)
   if(size != 0){
+    if(req.query.after == null){
+      throw new PokemonBadRequestMissingAfter()
+    }
     after = {id: {$gt: parseInt(req.query.after)}}
     count = parseInt(req.query.count)
-    pokemonModel.find(after).limit(count)
-    .then((doc)=>{
+    const doc = await pokemonModel.find(after).limit(count)
+    
       if (doc.length == 0){
-        return res.json({ errMsg : "Error : pokemon id should be between 1 and 809"});
+        throw new PokemonNotFoundError()
       } else {
         res.json(doc)
       }
-    })
-    .catch(err=>{
-      return res.json({errMsg: "Error : when getting a pokemon"})
-    })
-        }else {
-          pokemonModel.find({})
-              .then(doc => {
-                res.json(doc)
-               })
-              .catch(err => {
-                  console.error(err)
-                  res.json({ msg: "Error : returning pokemon" })
-          })
+   } else {
+          const doc = await pokemonModel.find({})
+          if(doc)
+            res.json(doc)
+          else
+            throw new PokemonBadRequest()
       }
-})
+      }
+))
 
 // app.post('/api/v1/pokemon')                      // - create a new pokemon
-app.post('/api/v1/pokemon', async (req,res)=>{
+app.post('/api/v1/pokemon', asyncWrapper(async (req,res)=>{
   var pokemonDex = req.body
   let product = await pokemonModel.exists({id: pokemonDex.id});
   
@@ -180,57 +169,50 @@ app.post('/api/v1/pokemon', async (req,res)=>{
   let pokeNameLeng = req.body.name.english.length;
 
   if (pokeNameLeng>20){
-    return res.json({errMsg : "ValidationError: check your ..."});
+    throw new PokemonNameLengthError("");
   }
 
   // var jsonLenth = parseInt(Object.keys(pokemonModel).length);
 
   //Check if there is an ID in the request body
-  if(pokemonDex.id == null){
-    return res.send({errMsg : 'The request body is invalid'});
+  if(!pokemonDex.id){
+    throw new PokemonBadRequestMissingID("");
   } else {
-    try{
       if(!pokemonDex || sizeOfObject === 0){
-        return res.send({errMsg : "Error : the request body is missing"});
+        throw new PokemonBadRequest("");
       }else if(product){
-        return res.json({ errMsg : "Error : the id is duplicated"});
+        throw new PokemonDuplicateError("");
       } else {
         pokemonModel.create(pokemonDex)
         .then((doc) => {
           return res.json({ msg: "post complited"})
         })     
       }
-    } catch(err){
-        console.log('err', err)
-        return res.json({errMsg: "Error : when creating a pokemon"})
-    }
   }
-})
+}))
 
 
 
 // app.get('/api/v1/pokemon/:id')                   // - get a pokemon
-app.get('/api/v1/pokemon/:id', (req,res)=>{
+app.get('/api/v1/pokemon/:id', asyncWrapper(async (req,res)=>{
   
   var id = req.params.id;
   // var regExp = /[a-zA-Z]/g;
 
   if(/[^0-9]/i.test(id)){
-    return res.json({errMsg: "Cast Error: pass pokemon id between 1 and 811"});
-  }else {
-    pokemonModel.findOne({id: id})
-    .then((doc)=>{
-      if (doc == null){
-        return res.json({ errMsg : "Error : pokemon id should be between 1 and 809"});
-      } else {
-        res.json(doc)
-      }
-    })
+   throw new PokemonCastError("")
   }
-});
+  const doc = await pokemonModel.findOne({id: id})
+  if(doc){
+    res.json(doc)
+  } else {
+    throw new PokemonNotFoundError("")
+  }
+  }
+));
 
 // app.get('/api/v1/pokemonImage/:id')              // - get a pokemon Image URL
-app.get('/api/v1/pokemonImage/:id', (req,res)=>{
+app.get('/api/v1/pokemonImage/:id', asyncWrapper(async (req,res)=>{
   var id = req.params.id;
   var newId;
   //Converts ID to three-character letters for use in image url
@@ -242,113 +224,67 @@ app.get('/api/v1/pokemonImage/:id', (req,res)=>{
     newId = id;
   }
 
-  pokemonModel.findOne({id: id})
-  .then((doc)=>{
+  const doc = await pokemonModel.findOne({id: id})
     if (doc == null){
-      res.json({value:"Error : pokemon id should be between 1 and 809"})
+      throw new PokemonNotFoundError("")
     } else {
       res.json({url:`https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${newId}.png`})
     }
-  })
-});
+  }));
 
 // app.put('/api/v1/pokemon/:id')                   // - upsert a whole pokemon document
-app.put('/api/v1/pokemon/:id', (req, res) => {
+app.put('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => {
   // upserts a whole pokemon document
   var inputId = parseInt(req.params.id);
   const { id, ...rest } = req.body;
-      pokemonModel.findOneAndUpdate({ id: inputId }, rest, { runValidators: true, new:true, upsert: true }, function (err) {
-      if (err) {
-          res.json({ errMsg: "Invalid value" })
-      } else {
-          res.json({ msg:"Updated or created Successfully"})
-      }
-      });
-}) 
+  const doc = await pokemonModel.findOneAndUpdate({ id: inputId }, rest, { runValidators: true, new:true, upsert: true })
+  console.log('doc ==') 
+  console.log(doc)
+    if(doc){
+      res.json({ msg:"Updated or created Successfully", pokeInfo: doc})
+    } else {
+      throw new PokemonNotFoundError("")
+    }
+}))
+
 
 // app.patch('/api/v1/pokemon/:id')                 // - patch a pokemon document or a portion of the pokemon document
-app.patch('/api/v1/pokemon/:id', async (req,res)=>{
+app.patch('/api/v1/pokemon/:id', asyncWrapper(async (req,res)=>{
   var inputId = parseInt(req.params.id);
   const { id, ...rest } = req.body;
 
-  var pokemon = await pokemonModel.find({id: inputId})
-  if (pokemon.length == 0) {
-    return res.json({errMsg: "this pokemon id is not exist"});
+  const doc = await pokemonModel.findOneAndUpdate({ id: inputId }, rest, { runValidators: true, new:true})
+  if (doc) {
+    res.json({
+      msg: "Updated Successfully",
+      pokeInfo: doc
+    })
+  } else {
+    
+    throw new PokemonNotFoundError("");
   }
-  pokemonModel.updateOne({id: inputId}, rest, {new : true, runValidators:true}, function(err){
-    if(err) {
-      return res.json({errMsg: "this pokemon id is not exist"});
-    } else {
-      return res.json({msg: "Udtated Successfully"});
-    }
-  })
-})
+}))
 
 // app.delete('/api/v1/pokemon/:id')                // - delete a  pokemon
-app.delete('/api/v1/pokemon/:id', (req,res)=>{
-  var id = parseInt(req.params.id);
-  pokemonModel.deleteOne({id:id}, function(err, result){
-    if (err) {
-      return res.json({errMsg: "Error : this pokemon id is not exist"});
-    } else {
-      if (result.deletedCount == 0){
-        return res.json({errMsg: "Error : this pokemon id is not exist"});
-      } else {
-        return res.json({msg: "Delete completed"});
-      }
-    }
-  })
-}) 
+app.delete('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => {
+
+    var id = parseInt(req.params.id);
+    const docs = await pokemonModel.deleteOne({id:id})
+    console.log(docs['deletedCount'])
+    console.log(docs)
+    if(docs['deletedCount'] == 0)
+      throw new PokemonNotFoundError("");
+    else
+          res.json({
+        msg: "Deleted Successfully"
+      })
+        
+}))
+
 // Improper Route
-// app.get('*', function (req, res) {
-//   res.json({msg: "Error : Improper route"});
-// })
-
-// app.get('/pokemonsAdvancedFiltering', function(req, res){
-//   res.json({msg: "ss"});
-
-// })
-
-  
-
-
-
-
-app.get('/pokemonsAdvancedFiltering', (req, res) => {
-  // console.log(comparisonOperators)
-
-    let filterArray = [];
-
-    if (req.query.comparisonOperators) {
-
-        const comparison = req.query.comparisonOperators.split(",");
-        console.log(comparison);
-        comparison.map(element => {
-            element = element.trim();
-
-            element = element.replace("<", "$lt");
-            element = element.replace("<=", "$lte");
-            element = element.replace(">", "$gt");
-            element = element.replace(">=", "$gte");
-            element = element.replace("==", "$eq");
-            element = element.replace("!=", "$ne");
-
-            const matches = element.match(regexForm);
-
-            const comparisonElement = {
-                ["base." + matches[1]]: {
-                    [matches[2]]: matches[3]
-                }
-            }
-            filterArray.push(comparisonElement);
-        })
-    }
-
-
-    pokemonModel.find({ $and: filterArray
-    }).exec((err, docs) => {
-        console.log(filterArray);
-        res.json(docs);
-    })
-
+app.get('*', function (req, res) {
+  // res.json({msg: "Error : Improper route"});
+  throw new PokemonNoSuchRouteError("");
 })
+
+app.use(handleErr)
---
+++ week8_lab/errors.js
@@ -0,0 +1,94 @@
+class PokemonBadRequest extends Error {
+    constructor(message) {
+      super(message);
+      this.name = 'PokemonBadRequest';
+      this.message = "Error - Bad request: check the API doc";
+      this.pokeErrCode = 400;
+    }
+  }
+  
+  class PokemonNotFoundError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = 'PokemonNotFoundError';
+      this.message = "Error - Pokemon was not found: check your request";
+      this.pokeErrCode = 400;
+    }
+  }
+  
+  class PokemonDbError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = 'PokemonDbError';
+      this.message = "Error - DB error: Contact API owners for more info.";
+      this.pokeErrCode = 500;
+    }
+  }
+  
+  class PokemonDuplicateError extends PokemonBadRequest {
+    constructor(message) {
+      super(message);
+      this.name = 'PokemonDuplicateError';
+      this.message = "Error - PokemonDuplicateError: The Pokemons has already been inserted.";
+      this.pokeErrCode = 400;
+    }
+  }
+  
+  class PokemonBadRequestMissingID extends PokemonBadRequest {
+    constructor(message) {
+      super(message);
+      this.name = 'PokemonBadRequestMissingID';
+      this.message = "Error - Bad request: check the API doc";
+      this.pokeErrCode = 400;
+    }
+  }
+  
+  class PokemonBadRequestMissingAfter extends PokemonBadRequest {
+    constructor(message) {
+      super(message);
+      this.name = 'PokemonBadRequestMissingAfter';
+      this.message = "Error - Bad request: check the API doc";
+      this.pokeErrCode = 400;
+    }
+  }
+  
+  class PokemonNoSuchRouteError extends PokemonBadRequest {
+    constructor(message) {
+      super(message);
+      this.name = 'PokemonNoSuchRouteError';
+      this.message = "Error - Improper Route: check the API doc";
+      this.pokeErrCode = 404;
+    }
+  }
+
+  class PokemonNameLengthError extends PokemonBadRequest {
+    constructor(message) {
+      super(message);
+      this.name = 'PokemonNameLengthError';
+      this.message = "Error - Improper Pokemon Name: check the pokemon name length";
+      this.pokeErrCode = 400;
+    }
+  }
+
+  class PokemonCastError extends PokemonBadRequest {
+    constructor(message) {
+      super(message);
+      this.name = 'PokemonCastError';
+      this.message = "Cast Error - Improper Pokemon Number: check the pokemon number type";
+      this.pokeErrCode = 400;
+    }
+  }
+  
+  module.exports = {
+    PokemonBadRequest,
+    PokemonBadRequestMissingID,
+    PokemonBadRequestMissingAfter,
+    PokemonDbError,
+    PokemonNotFoundError,
+    PokemonDuplicateError,
+    PokemonNoSuchRouteError,
+    PokemonNameLengthError,
+    PokemonCastError
+
+  };
+  
