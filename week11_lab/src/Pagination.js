0ed945347652944f7157ae000ff8ac696616a278
Damon-Jo <lunaticjky@gmail.com>
2022-11-27 01:45:59 -0800

    copied template based on lab10

---
+++ week11_lab/src/Pagination.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css';
+
+function Pagination({ numberOfPages, currentPage, setCurrentPage }) {
+  const pageNumbers = []
+  for (let index = 1; index <= numberOfPages; index++) {
+    pageNumbers.push(index)
+  }
+  const nextPage = () => {
+    toggleActive(currentPage)
+    if (currentPage !== numberOfPages) setCurrentPage(currentPage + 1)
+    if(currentPage + 1 === numberOfPages) setNextBtnActive(false)
+  }
+  const prevPage = () => {
+    toggleActive(currentPage-2)
+    if (currentPage !== 1) setCurrentPage(currentPage - 1)
+    if(currentPage - 1 === 1) setPreBtnActive(false)
+  }
+
+let [preBtnActive, setPreBtnActive] = useState(false);
+let [nextBtnActive, setNextBtnActive] = useState(true);
+
+
+const isFirstPage = (index)=>{
+  var pageNumber = index + 1;
+  if(pageNumber === 1){
+    setPreBtnActive(false);
+  } else {
+    setPreBtnActive(true);
+  }
+}
+
+const isLastPage = (index)=>{
+  var pageNumber = index + 1;
+  if(pageNumber === numberOfPages){
+    setNextBtnActive(false);
+  } else {
+    setNextBtnActive(true);
+  }
+}
+
+let [btnActive, setBtnActive] = useState("");
+
+const toggleActive = (e) => {
+  setBtnActive((prev) => {
+    return e;
+  });
+};
+
+  return (
+    <div>
+      {
+        preBtnActive == true ? <button onClick={prevPage}>prev </button> : null
+      }
+      {
+        pageNumbers.map((number, i) => {
+          if (number < currentPage + 6 && number > currentPage - 6)
+            return (
+            <>
+              <button
+              
+                value={i}
+                className={(i == btnActive ? "active" : "")}
+                onClick={() => [toggleActive(i), setCurrentPage(number), isFirstPage(i), isLastPage(i)]}
+                >
+                {number}
+              </button>
+            </>)
+        })
+      }
+      {
+        nextBtnActive == true ? <button  onClick={nextPage}>next</button> : null
+      }
+    </div>
+  )
+}
+
+export default Pagination
