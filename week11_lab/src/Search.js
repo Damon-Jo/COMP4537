73777e5e6da4dba55ebc0816952ffa07842ab655
Damon-Jo <lunaticjky@gmail.com>
2022-11-27 01:58:11 -0800

    created Search Component

---
+++ week11_lab/src/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <div>
+        <span>
+            <input type='checkbox'/>
+            <label>type</label>
+
+        </span>
+
+    </div>
+  )
+}
+
+export default Search
